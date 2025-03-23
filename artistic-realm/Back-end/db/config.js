45189179fc86c91cb9eebc266073f66b9472bcfe
Sujeet-sharma786 <svishwakarma6389@gmail.com>
2025-03-23 19:46:04 +0530

    'strictQuery-added'

--- artistic-realm/Back-end/db/config.js
+++ artistic-realm/Back-end/db/config.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 // mongoose.connect("mongodb://127.0.0.1:27017/paintingworld");
+mongoose.set("strictQuery", true);  // Enforce strict query validation
 
 const connectMongoDB = () => {
   try {
